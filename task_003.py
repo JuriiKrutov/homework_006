1bda7bf92c5699650cd6da7e3a6d67fced496cb0
JuriiKrutov <juriikrutov@gmail.com>
2023-01-20 20:25:21 +0300

    task_003

---
+++ task_003.py
@@ -0,0 +1,13 @@
+'''Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень
+B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8'''
+
+def pow_a_b(a, b, res):
+    if b == 1:
+        return res
+    return pow_a_b(a, b -1, res * a)
+
+a, b = int(input()), int(input())
+res = a
+print(pow_a_b(a, b, res))
